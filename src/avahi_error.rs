5b15ba3ef89108d1f9c2c9b85aa105663474cff3
Lutz <727209+lurtz@users.noreply.github.com>
2025-06-16 13:13:35 +0200

    remove compiler suppressions

--- src/avahi_error.rs
+++ src/avahi_error.rs
@@ -6,9 +6,7 @@ use std::io;
 #[derive(Debug)]
 pub enum Error {
     NoHostsFound,
-    #[allow(dead_code)] // used in Display implementation
     IO(io::Error),
-    #[allow(dead_code)] // used in Display implementation
     Zeroconf(zeroconf::error::Error),
 }
 
