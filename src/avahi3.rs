e700e2237b1b5a2caa37616f4f020112954590fc
Lutz <727209+lurtz@users.noreply.github.com>
2025-06-16 12:06:45 +0200

    Apply cargo clippy --fix

--- src/avahi3.rs
+++ src/avahi3.rs
@@ -33,30 +33,27 @@ fn get_hostname(service_type: ServiceType, logger: &dyn Logger) -> Result<Servic
     {
         match event_loop.poll(Duration::from_secs(0)) {
             Ok(_) => {}
-            Err(x) => logger.log(&format!("{}", x)),
+            Err(x) => logger.log(&format!("{x}")),
         }
     }
 
-    let result = match &context.lock().unwrap().service_discovery {
+    
+    match &context.lock().unwrap().service_discovery {
         Some(x) => Ok(x.clone()),
         None => Err(Error::NoHostsFound),
-    };
-    result
+    }
 }
 
 fn on_service_discovered(
     result: zeroconf::Result<ServiceDiscovery>,
     context: Option<Arc<dyn Any>>,
 ) {
-    if let Ok(sd) = result {
-        if let Some(ctx) = context {
-            if let Some(m) = ctx.downcast_ref::<Arc<Mutex<Context>>>() {
-                if let Ok(mut ctx) = m.lock() {
+    if let Ok(sd) = result
+        && let Some(ctx) = context
+            && let Some(m) = ctx.downcast_ref::<Arc<Mutex<Context>>>()
+                && let Ok(mut ctx) = m.lock() {
                     ctx.service_discovery = Some(sd);
                 }
-            }
-        }
-    }
 }
 
 fn get_roap_service_type() -> ServiceType {
